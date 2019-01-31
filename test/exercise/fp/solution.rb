85a66b63f84020cc31f299429aaa1b309bacc1a9
Vadim Valitov <vadim.valitov@gmail.com>
2019-01-31 13:16:06 +0400

    Fp tests

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,11 +5,12 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        filtered = _array.map { |film| film['rating_kinopoisk'].to_f if film['rating_kinopoisk'].to_f > 0 && film['country'].to_s.split(',').length >= 2 }.compact
+        filtered.reduce(:+) / filtered.length
       end
 
       def chars_count(_films, _threshold)
-        0
+        _films.map { |film| film['name'].scan('и').length if film['rating_kinopoisk'].to_f > _threshold }.compact.reduce(:+)
       end
     end
   end
